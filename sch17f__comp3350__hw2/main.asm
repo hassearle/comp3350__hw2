9211ce7b5da9c0884ed199f384bbb323100dd3ec
Unknown <a.searle274@gmail.com>
2017-09-18 10:49:33 -0500

    skeleton code / add(1)

--- sch17f__comp3350__hw2/main.asm
+++ sch17f__comp3350__hw2/main.asm
@@ -8,13 +8,21 @@
 ExitProcess proto,dwExitCode:dword
 
 .data
-
-;PUT DATA HERE
+	input byte 7,6,5,4,3,2,1,0
+	shift byte 1
 
 .code
 	main proc
-
-;PUT CODE HERE
+		mov eax, 0
+		mov ebx, 0
+		mov ecx, 0
+		mov edx, 0
+		
+		mov al, input[0]
+		mov ax, input[1]
+		add ax, al
+		
 	
+	invoke ExitProcess, 0
 	main endp
 end main
