b06c39e4f0f36b4c8c77c05b2ca60be79b4675f8
Unknown <a.searle274@gmail.com>
2017-09-18 10:21:35 -0500

    template

---
+++ sch17f__comp3350__hw2/main.asm
@@ -0,0 +1,20 @@
+;Author: Ash Searle
+;Username: kss0024
+;Homework #2
+
+.386
+.model flat,stdcall
+.stack 4096
+ExitProcess proto,dwExitCode:dword
+
+.data
+
+;PUT DATA HERE
+
+.code
+	main proc
+
+;PUT CODE HERE
+	
+	main endp
+end main
